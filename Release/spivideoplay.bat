fe05c01cb0bdf0a42821de3daa08718a48d6c854
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-13 17:54:44 -0500

    first commit

---
+++ Release/spivideoplay.bat
@@ -0,0 +1,19 @@
+rem start spivideoplay.exe "baraka_chickens(1).avi" 4.0 1920 0 1920 1080 220 0 0 0
+rem start spivideoplay.exe "audio_spi Core Engineering Report 0002.m4v" 360.0 100 100 200 200 255 0 0 0
+rem start spivideoplay.exe "audio_spi Core Engineering Report 0001.m4v" 360.0 1920 0 200 200 220 0 0 0
+rem start spivideoplay.exe "baraka_chickens(1).avi" 4.0 0 0 1920 1080 210 0 0 0
+rem start spivideoplay.exe "jesus-bd_pages.avi" 8.0 0 0 1920 1080 150 0 0 0
+rem start spivideoplay.exe "baraka_african-ritual.avi" 8.0 0 0 1920 1080 255 0 0 0
+rem start spivideoplay.exe "baraka_african-ritual.avi" -1.0 0 0 1920 1080 255 0 0 0
+rem start spivideoplay.exe "I:\mvi_0005.avi" 8.0 0 0 1920 1080 255 0 0 0
+rem start spivideoplay.exe "mvi_0004.avi" 30.0 0 0 500 500 255 0 0 0
+rem start spivideoplay.exe "Countdown1960_512kb.mp4" 15.0 1 1 1900 1000 255 0 0 0
+rem start spivideoplay.exe "C:\video\video_spi-projects\mtl-surf-with-roxy\GOPR0073.mp4" 15.0 1 1 1900 1000 255 0 0 0
+rem start spivideoplay.exe "spiffmpegmuxing-output.mp4" -1.0 0 0 1920 1080 255 0 0 0
+rem start spivideoplay.exe "I:\znnz_000002.m4v" 8.0 0 0 1920 1080 255 0 0 0
+rem start spivideoplay.exe "I:\mvi_0021.avi" 30.0 0 0 1920 1080 255 0 0 0
+rem start spivideoplay.exe "baraka_african-ritual.avi" 8.0 1920 0 1920 1080 220 0 0 0
+rem start spivideoplay.exe "D:\mirror-1TB\audio_video\FEATURE_EXTRACTS\baraka_eclipse.avi" 8.0 1920 0 1920 1080 220 0 0 0
+rem start spivideoplay.exe "D:\mirror-1TB\audio_video\FEATURE_EXTRACTS\monnomestpersonne_peche.avi" 4.0 1920 0 1920 1080 220 0 0 0
+start spivideoplay.exe "C:\video\video_spi-projects\dj-oifii_percpattern(Loreen - Euphoria)_002.mp4" 15.0 1 1 1900 1000 255 0 0 0
+rem start spivideoplay.exe "C:\video\video_spi-projects\dj-oifii_percpattern(Loreen - Euphoria)_002.flv" 15.0 1 1 1900 1000 255 0 0 0
